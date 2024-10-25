35bfb5b14e3e658169beb6db0c1f993ce9ff99c7
Denish Tomar <152975472+Denish3436@users.noreply.github.com>
2024-10-25 22:20:55 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,20 @@
+package config
+
+import (
+    "os"
+)
+
+type Config struct {
+    Port        string
+    DBConnection string
+    CacheTTL    int
+}
+
+func GetConfig() *Config {
+    return &Config{
+        Port:        os.Getenv("PORT"), // Read PORT from environment variables
+        DBConnection: os.Getenv("DB_CONNECTION"), // Database connection string
+        CacheTTL:    60,
+    }
+}
 
