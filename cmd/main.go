676aaa7872e4d1094fe26b46caac00dbd726c24b
Denish Tomar <152975472+Denish3436@users.noreply.github.com>
2024-10-30 23:05:36 +0530

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
 
+package main
+
+import (
+    "log"
+    "url-shortener/config"
+)
+
+func main(
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    cfg := config.GetConfig()
+
+    db, err := sql.Open("postgres", cfg.DBConnection)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
